20c9b4d8910fc7172660a9d90397cc634af4a497
ChanghunHan <ehwjs1914@naver.com>
2025-10-08 13:58:12 +0900

    change mobilenet v2 models path

--- tests/classification_seg_test.py
+++ tests/classification_seg_test.py
@@ -7,10 +7,13 @@ from segments_runner import SegmentsRunner
 def main():
     image_path = "segments_runner/test_data/parrot.jpg"
     segment_paths = [
-        "models/mobilenet_v2_1.0_224_inat_bird_quant_segment_0_of_3_edgetpu.tflite",
-        "models/mobilenet_v2_1.0_224_inat_bird_quant_segment_1_of_3_edgetpu.tflite",
-        "models/mobilenet_v2_1.0_224_inat_bird_quant_segment_2_of_3_edgetpu.tflite",
+        "models/mobilenet_v2_segment_0_of_3_edgetpu.tflite",
+        "models/mobilenet_v2_segment_1_of_3_edgetpu.tflite",
+        "models/mobilenet_v2_segment_2_of_3_edgetpu.tflite",
     ]
+    # segment_paths = [
+    #     "models/mobilenet_v2_edgetpu.tflite",
+    # ]
 
     runner = SegmentsRunner(segment_paths)
 
@@ -25,9 +28,9 @@ def main():
         start = time.perf_counter()
         runner.invoke_all()
         inference_time = time.perf_counter() - start
+        print("%.1fms" % (inference_time * 1000))
 
     classes_dict = runner.get_result(top_n=5, detection=False)
-    print("%.1fms" % (inference_time * 1000))
 
     print("-------RESULTS--------")
     for label, score in classes_dict.items():
